b9f21d284fb7a68360d70d3974ae9ab85bd65d51
Esteban Gonzalez <estebangonzalez@seven4n.com>
2021-03-10 12:27:44 -0500

    Se añade documentación a User.scala, UserRepositoryAlgebra.scala

--- src/main/scala/co/s4ncampus/fpwithscala/users/domain/User.scala
+++ src/main/scala/co/s4ncampus/fpwithscala/users/domain/User.scala
@@ -1,5 +1,10 @@
 package co.s4ncampus.fpwithscala.users.domain
 
+/**
+  * @constructor      Construye la clase User
+  * @param id         Identificación única del usuario generada aleatoriamente
+  */
+
 case class User(
     id: Option[Long],
     legalId: String,
--- src/main/scala/co/s4ncampus/fpwithscala/users/domain/UserRepositoryAlgebra.scala
+++ src/main/scala/co/s4ncampus/fpwithscala/users/domain/UserRepositoryAlgebra.scala
@@ -3,6 +3,22 @@ package co.s4ncampus.fpwithscala.users.domain
 import cats.data.OptionT
 
 trait UserRepositoryAlgebra[F[_]] {
+  /**
+    * Definición del comportamiento de cración de Users
+    * @param user
+    * @return Promesa de creación de User
+    */
   def create(user: User): F[User]
+
+  /**
+    * Deficinición del comportamiento de busqueda de usuario
+    * @param legalId Documento de identificación del usuario
+    * @return
+    */
   def findByLegalId(legalId: String): OptionT[F, User]
+
+  /**
+    * @todo delete
+    * @todo update
+    */
 }
